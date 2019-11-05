359a63281a189a4269a1eff1b28491bae4c0dff9
Simon Walker <s.r.walker101@googlemail.com>
2019-11-05 22:51:40 +0000

    Create hook package

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+#[macro_use]
+extern crate tower_web;
+
+use serde_json::Value;
+use tower_web::middleware::log::LogMiddleware;
+use tower_web::ServiceBuilder;
+
+struct MyApp;
+
+impl_web! {
+    impl MyApp {
+        #[get("/")]
+        fn index(&self) -> Result<&'static str, ()> {
+            Ok("hello")
+        }
+
+        #[post("/hook")]
+        fn hook(&self, body: Value) -> Result<&'static str, ()> {
+            println!("{:#?}", body);
+            Ok("ok")
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _ = env_logger::try_init();
+
+    let addr = "0.0.0.0:9050".parse().expect("Invalid address");
+    println!("Listening on http://{}", addr);
+
+    ServiceBuilder::new()
+        .resource(MyApp)
+        .middleware(LogMiddleware::new("hello_world::web"))
+        .run(&addr)
+        .unwrap();
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_my_app() {
+        let app = MyApp {};
+        assert_eq!(app.index(), Ok("hello"));
+    }
 }
